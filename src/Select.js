d766be0bc1b405f774e96f56f1e9df4b9b3fd476
Eric Soderberg <eric.soderberg@hpe.com>
2018-03-01 21:34:11 -0700

    Enhanced Select to include search

--- src/Select.js
+++ src/Select.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import { Select } from 'grommet';
 import SandboxComponent from './SandboxComponent';
 
+const OPTIONS = ['First', 'Second', 'Third'];
+
 export default class extends Component {
-  state = { value: '' }
+  state = { value: '', options: OPTIONS }
 
   render() {
-    const { value } = this.state;
+    const { options, value } = this.state;
     return (
       <SandboxComponent>
         <Select
           value={value}
-          onChange={event => this.setState({ value: event.option })}
-          options={['First', 'Second', 'Third']}
+          onSearch={(searchText) => {
+            const regexp = new RegExp(searchText, 'i');
+            this.setState({ options: OPTIONS.filter(o => o.match(regexp)) });
+          }}
+          onChange={event => this.setState({
+            value: event.option,
+            options: OPTIONS,
+          })}
+          options={options}
         />
       </SandboxComponent>
     );
