b885ee210799959f03ef4ee41ca51fbc5485c572
britt6612 <42451602+britt6612@users.noreply.github.com>
2019-02-22 15:15:45 -0700

    Update ResponsiveContextConsumer.js
    Added the theme for the font to change

--- src/ResponsiveContextConsumer.js
+++ src/ResponsiveContextConsumer.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import { Box, ResponsiveContext, Text } from 'grommet';
+import { deepMerge } from 'grommet/utils';
+import { grommet } from 'grommet/themes';
+import { Box, Grommet, Heading, ResponsiveContext } from 'grommet';
 import SandboxComponent from './SandboxComponent';
 
+const customBreakpoints = deepMerge(grommet, {
+  global: {
+    breakpoints: {
+      xsmall: {
+        value: 500,
+      },
+      small: {
+        value: 900,
+      },
+      medium: undefined,
+      middle: {
+        value: 2000,
+      },
+    },
+  },
+});
 export default class extends Component {
   render() {
     return (
       <SandboxComponent>
-        <ResponsiveContext.Consumer>
-          {size => (
-            <Box>
-              <Text>This size will change depending on your screen size {size}
-              </Text>
-            </Box>
+        <Grommet theme={customBreakpoints}>
+          <ResponsiveContext.Consumer>
+            {size => (
+              <Box>
+                <Heading>{`This size will change depending on your screen size ${size}`}
+                </Heading>
+              </Box>
           )}
-        </ResponsiveContext.Consumer>
+          </ResponsiveContext.Consumer>
+        </Grommet>
       </SandboxComponent>
     );
   }
