0228985c923240353606b4d85fb3d55f448944f3
shimisun <shimrit.yacobi@gmail.com>
2019-03-10 23:20:45 -0600

    fix Form and FormFields usage

--- src/Form.js
+++ src/Form.js
@@ -1,33 +1,55 @@
-import React from 'react';
-import { Box, Button, CheckBox, FormField, RadioButton, Select, TextArea, TextInput } from 'grommet';
-import SandboxComponent from './SandboxComponent';
+import React from "react";
+import {
+  Box,
+  Button,
+  CheckBox,
+  Form,
+  FormField,
+  RadioButton,
+  Select
+} from "grommet";
+import SandboxComponent from "./SandboxComponent";
+
+const RadioButtonGroup = ({ name, onChange, options, value }) => (
+  <Box gap="small">
+    {options.map(option => (
+      <Box key={option}>
+        <RadioButton
+          name={name}
+          value={option}
+          label={option}
+          checked={value === option}
+          onChange={() => onChange({ value: option })}
+        />
+      </Box>
+    ))}
+  </Box>
+);
 
 export default () => (
-  <SandboxComponent pad='none'>
-    <form onSubmit={event => event.preventDefault()}>
-      <Box>
-        <FormField label='Name'>
-          <TextInput />
-        </FormField>
-        <FormField label='Continent'>
-          <Select options={['North America', 'South America']} />
-        </FormField>
-        <FormField label='Address'>
-          <TextArea />
-        </FormField>
-        <FormField>
-          <Box pad='small'>
-            <CheckBox label='acknowledge' />
-          </Box>
-        </FormField>
-        <FormField>
-          <Box pad='small' gap='xsmall'>
-            <RadioButton name='size' id='small' label='small' />
-            <RadioButton name='size' id='large' label='large' />
-          </Box>
-        </FormField>
-        <Button type='submit' label='Submit' primary={true} />
-      </Box>
-    </form>
+  <SandboxComponent pad="none">
+    <Form onSubmit={({ value }) => console.log("Submit: ", value)}>
+      <FormField name="name" label="Name" required />
+      <FormField
+        label="Continent"
+        name="select"
+        component={Select}
+        options={["North America", "South America", "Asia"]}
+      />
+      <FormField name="address" label="Address" />
+      <FormField
+        name="acknowledge"
+        component={CheckBox}
+        pad
+        label="acknowledge"
+      />
+      <FormField
+        name="day"
+        component={RadioButtonGroup}
+        pad
+        options={["Tuesday", "Friday"]}
+      />
+      <Button type="submit" label="Submit" primary />
+    </Form>
   </SandboxComponent>
 );
