4685f85fc9bee424efa7a2c55c18062128217193
shimisun <shimrit.yacobi@gmail.com>
2019-03-10 23:23:06 -0600

    add prettier to linter

--- src/Form.js
+++ src/Form.js
@@ -6,7 +6,7 @@ import {
   Form,
   FormField,
   RadioButton,
-  Select
+  Select,
 } from "grommet";
 import SandboxComponent from "./SandboxComponent";
 
@@ -29,7 +29,7 @@ const RadioButtonGroup = ({ name, onChange, options, value }) => (
 export default () => (
   <SandboxComponent pad="none">
     <Form onSubmit={({ value }) => console.log("Submit: ", value)}>
-      <FormField name="name" label="Name" required />
+      <FormField name="name" label="Name" required={true} />
       <FormField
         label="Continent"
         name="select"
@@ -40,16 +40,16 @@ export default () => (
       <FormField
         name="acknowledge"
         component={CheckBox}
-        pad
+        pad={true}
         label="acknowledge"
       />
       <FormField
         name="day"
         component={RadioButtonGroup}
-        pad
+        pad={true}
         options={["Tuesday", "Friday"]}
       />
-      <Button type="submit" label="Submit" primary />
+      <Button type="submit" label="Submit" primary={true} />
     </Form>
   </SandboxComponent>
 );
